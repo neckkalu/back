1db10b3641cbe2ed2234e6ec40615f4b3987e3fd
C:/Program Files/Git/ <leekm0817@naver.com>
2023-09-25 16:33:32 +0900

    jwtprovider

--- src/main/java/com/htbuddy/htbuddyback/provider/JwtProvider.java
+++ src/main/java/com/htbuddy/htbuddyback/provider/JwtProvider.java
@@ -2,6 +2,7 @@ package com.htbuddy.htbuddyback.provider;
 
 import org.springframework.stereotype.Component;
 
+import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 
@@ -17,13 +18,30 @@ public class JwtProvider {
 
   public String create(String email) {
 
-    //asdafqwdhjnkasdjk
-    //////asdasd
     Date expiredDate = Date.from(Instant.now().plus(1, ChronoUnit.HOURS));
 
-    String jwt = Jwts.builder().signWith(SignatureAlgorithm.HS256, secretKey).setSubject(email).setIssuedAt(new Date()).setExpiration(expiredDate).compact();
+    String jwt = Jwts.builder()
+        .signWith(SignatureAlgorithm.ES256, secretKey)
+        .setSubject(email). setIssuedAt(new Date()).setExpiration(expiredDate)
+        .compact();
 
     return jwt;
+
+  }
+
+  public String validate(String jwt) {
+
+    Claims claims = null;
+
+    try {
+        claims = Jwts.parser().setSigningKey(secretKey)
+            .parseClaimsJwt(jwt).getBody();
+    } catch (Exception exception) {
+      exception.printStackTrace();
+      return null;
+    }
+
+    return claims.getSubject();
 
   }
 
