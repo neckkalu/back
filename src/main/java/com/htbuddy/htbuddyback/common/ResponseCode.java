8eac2ddc9f3db8bb10384de04a4624707168fd9e
neckkalu <ckswls1265@gmail.com>
2023-09-25 17:50:15 +0900

    ResponseCode&Message

---
+++ src/main/java/com/htbuddy/htbuddyback/common/ResponseCode.java
@@ -0,0 +1,26 @@
+package com.htbuddy.htbuddyback.common;
+
+public interface ResponseCode {
+
+  // HTTP Status 200 (성공)
+  String SUCCESS = "SU";
+
+  // HTTP Status 400
+  String VALIDATION_FAILED = "VF";
+  String EXISTED_EMAIL = "EE";
+  String EXISTED_TELNUMBER = "ET";
+  String EXISTED_NICKNAME = "EN";
+  String NO_EXISTED_BOARD_NUMBER = "NB";
+  String NO_EXISTED_USER = "NU";
+  String NO_EXISTED_CHATTINGROOM = "NC";
+
+  // HTTP Status 401
+  String SIGN_IN_FAIL = "SF";
+  String AUTHORIZATION_FAIL = "AF";
+
+  // HTTP Status 403
+  String NO_PERMISSION = "NP";
+
+  // HTTP Status 500
+  String DATABASE_ERROR = "DE"
+}
