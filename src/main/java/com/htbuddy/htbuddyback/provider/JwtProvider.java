2f330e2b853b58beebf6aa86047fa7d155973d02
neckkalu <ckswls1265@gmail.com>
2023-09-25 16:10:01 +0900

    commit for pull

--- src/main/java/com/htbuddy/htbuddyback/provider/JwtProvider.java
+++ src/main/java/com/htbuddy/htbuddyback/provider/JwtProvider.java
@@ -17,6 +17,7 @@ public class JwtProvider {
 
   public String create(String email) {
 
+    //asdafqwdhjnkasdjk
     Date expiredDate = Date.from(Instant.now().plus(1, ChronoUnit.HOURS));
 
     String jwt = Jwts.builder().signWith(SignatureAlgorithm.HS256, secretKey).setSubject(email).setIssuedAt(new Date()).setExpiration(expiredDate).compact();
